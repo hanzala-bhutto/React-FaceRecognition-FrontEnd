e820748bb893bfa9e9d45330b07ea952d31f713f
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-02-04 15:35:32 +0500

    Rank component structure set

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,14 @@
+const Rank = () => {
+    return (
+        <div>
+            <div className="center white f3">
+                Hero your current entry count is ...
+            </div>
+            <div className="white f1">
+                #5
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
