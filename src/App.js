03a9b706e6a999d6c6085330af0c33a1bf4ce585
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-02-04 14:59:17 +0500

    Logo component Structure set and brain img incorporated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
-      <Rank />
+      <Logo />
+      {/* <Rank />
       <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
