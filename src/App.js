2353618ec15c7f62747941c9bd4efeb90c2145f7
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-02-04 15:44:29 +0500

    set imagelinkform responsive using media query

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ParticlesBg from 'particles-bg';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
@@ -7,6 +8,7 @@ import Rank from './Components/Rank/Rank';
 function App() {
   return (
     <div className="App">
+      <ParticlesBg color="#ffffff" num={100} alpha={[0.9, 0]} type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
