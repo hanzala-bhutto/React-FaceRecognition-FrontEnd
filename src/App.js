b3c3bd0500d75fe9d32a0ba6783978972553601c
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-02-04 14:41:50 +0500

    necessary packages installed, main app css structure set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Navigation from './Components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      {/* <Logo />
+      <Rank />
+      <ImageLinkForm />
+      <FaceRecognition /> */}
     </div>
   );
 }
