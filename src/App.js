c8412487ab0b74cdb61c487cef9e249c3ff1c0ba
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-02-05 10:21:08 +0500

    fixed bugs in fetch requests

--- src/App.js
+++ src/App.js
@@ -61,13 +61,33 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    console.log('click');
-    this.setState({imageUrl:this.state.input});
+    // console.log('click');
 
-    
-    //   .catch(err => console.log(err));
-
-
+    fetch('http://localhost:3000/imageurl', {
+      method: 'put',
+      headers : {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input:this.state.input
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      if(response){
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, {entries:count}))
+        })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(err => console.log(err));
   }
 
   onRouteChange = (route) => {
--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -19,7 +19,7 @@ onPasswordChange = (event) => {
 }
 
 onSubmitSignIn = () => {
-  fetch("http:localhost:3000/signin", {
+  fetch("http://localhost:3000/signin", {
     method:'post',
     headers:{'Content-Type': 'application/json'},
     body : JSON.stringify({
